f39f67a91966b51e816afb37aabf9ddcc89bbdfb
amirbjit <amir.shohag@bjitgroup.com>
2023-01-31 15:56:16 +0600

    Add project files.

---
+++ HRM.API/Db/HRMContext.cs
@@ -0,0 +1,16 @@
+﻿using HRM.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace HRM.API.Db
+{
+    public class HRMContext
+    {
+        public HRMContext(DbContextOptions<HRMContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
